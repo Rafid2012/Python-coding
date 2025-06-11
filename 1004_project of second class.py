f7ca926825eb9e5434ffc03fce28246c478fe888
Rafid2012 <hasanndc2000@gmail.com>
2025-06-11 16:00:38 +0600

    Add files via upload

---
+++ 1004_project of second class.py
@@ -0,0 +1,5 @@
+a = input("Enter your birthday: ")
+b = input("Enter your mother's birthday: ")
+c = input("Enter your father's birthday: ")
+
+print(f"My birthday is {a}. My mother's birthday is {b}. My fathers birthday is {c}. ")
