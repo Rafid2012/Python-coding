ea3bd8e099311231c8f3c60a52cf50c23e83e3dd
Rafid2012 <hasanndc2000@gmail.com>
2025-06-12 15:42:46 +0600

    Add files via upload

---
+++ 1005_Type casting and Arethmetic operator.py
@@ -0,0 +1,8 @@
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+
+print(f"addition: {a+b}")
+print(f"substraction: {a-b}")
+print(f"multiplication: {a*b}")
+print(f"division: {a/b}")
+print(f"modulus: {a%b}")
---
+++ 1006_str opration.py
@@ -0,0 +1,23 @@
+# count operation
+txt = "I love apples, apple are my favorite fruit"
+
+x = txt.count("p")
+
+print(x)
+
+# endswith operation
+txt = "Hello, welcome to my world"
+
+x = txt.endswith(".")
+
+print(x)
+
+#find opration
+
+txt = "Hello, welcome to my world."
+
+x = txt.find("welcome")
+
+print(x)
+
+
