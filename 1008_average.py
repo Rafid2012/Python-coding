b8f9f47503fd750f23ef67c8a5d4613bf866e33b
Rafid2012 <hasanndc2000@gmail.com>
2025-06-26 18:50:18 +0600

    Add files via upload

---
+++ 1008_average.py
@@ -0,0 +1,11 @@
+a= int(input("Enter your first number: "))
+b= int(input("Enter your second number: "))
+c= int(input("Enter your third number: "))
+d= int(input("Enter your fourth number: "))
+e= int(input("Enter your fifth number: "))
+
+total = a+b+c+d+e
+
+avg = total // 5
+
+print("Average: ",avg)
