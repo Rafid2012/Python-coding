281471b7ee009108c7f4ca98caae992ae3a8f1e6
Rafid2012 <hasanndc2000@gmail.com>
2025-07-04 18:00:39 +0600

    Add files via upload

---
+++ 1028_project 8 number swipe.py
@@ -0,0 +1,8 @@
+num1= int(input("Enter your first number: "))
+num2= int(input("Enter your second number: "))
+
+print(f"before swap: {num1}, {num2}")
+num1,num2= num2,num1
+print(f"after swap: {num1}, {num2}")
+
+
