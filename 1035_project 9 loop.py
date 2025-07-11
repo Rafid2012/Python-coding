5401cefdba2aa7dc44b1466f307a5d6d3b685d3d
Rafid2012 <hasanndc2000@gmail.com>
2025-07-11 18:22:10 +0600

    Add files via upload

--- 1035_project 9 loop.py
+++ 1035_project 9 loop.py
@@ -1,8 +1,11 @@
-a = int(input("Enter the last number: "))
+a = int(input("Enter the number: "))
 
 s = 0
 
 for i in range(a+1):
-    s = s + i*i
-print(f"RESULT: {s}")
+    s= s + i*i
+print(f"Result: {s}")
+
+
+   
 
