092a8b1e2d95a58b245d2747ae28b230eb442a51
Rafid2012 <hasanndc2000@gmail.com>
2025-08-22 23:50:23 +0600

    Add files via upload

---
+++ 1065_Trigonometic function.py
@@ -0,0 +1,12 @@
+import math
+
+d = 90
+r = math.pi / 4
+
+s = math.sin(r)
+c = math.cos(r)
+t = math.tan(r)
+
+print(f"Sine of {d} degrees:{s}")
+print(f"Cosine of {d} degrees:{c}")
+print(f"Tangent of {d} degrees:{t}")
